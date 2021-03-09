f7be15cae9ddf11d1f2a52c3d0cfcacb8977f43c
Curry <curry_dockins@yahoo.com>
2021-03-09 16:35:13 -0500

    Update Program.cs
    Updated a lot in here. Still not clean but it works for the most part. Still missing a few checks on input but it looks more complete.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,59 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace MethodTraining
 {
+    public static class Extensions
+    {
+        public static bool find<T>(this T[] array, T target)
+        {
+            return array.Contains(target);
+        }
+    }
+    public static class ByteStuff
+    {
+
+        public static byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+        public static string StringToInt32Ready(String hexString)
+        {
+            int[] arrayInt1 = { 7, 8 };                         //----------------------------//
+            int[] arrayInt2 = { 3, 4 };                         // Manually converting user   //
+            int lengthOfByte = hexString.Length;                // input to match 8           //
+            bool byteSize = arrayInt1.find(lengthOfByte);       // bytes for later conversion //
+            if (!byteSize && hexString.Length <= 8)             //----------------------------//
+            {
+                byteSize = arrayInt2.find(lengthOfByte);
+                if (!byteSize && hexString.Length < 4)
+                {
+                    if (hexString.Length == 0)
+                    {
+                        hexString += "00";
+                    }
+                    hexString += "0000";
+                }
+                else
+                {
+                    hexString += "00";
+                }
+                hexString += "00";
+            }
+            return hexString;
+        }
+    }
     class Program
     {
+        void ByteWriting(byte[] bytes)
+        {
+
+        }
         void MethodWriting(string fileName)                //This method is for writing data
         {
             FileStream File = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
@@ -52,7 +101,7 @@ namespace MethodTraining
                     Console.Write(hex + " ");
                     if (y == 0x10)
                     {
-                        Console.WriteLine("\r");
+                        Console.Write("\n");
                         y -= 0x10;
                     }
                     y++;
@@ -83,38 +132,72 @@ namespace MethodTraining
             FileStream editFile = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
             {
                 Console.WriteLine("File Found.");
-                string input = "";
-                string value = "";
-                int valueInt;
-                while (input != "4")
-                Console.WriteLine("Please select from the following options.\n1. Read single byte.\n2. Edit single byte.\n3. Add bytes to end of file.\n4. Quit");
-                input = Console.ReadLine();
-                if (input == "1")
+                string input;
+                string value;
+                byte inputInt;
+                byte[] valueByte;
+                byte[] inputByte;
+                bool loop = true;
+                do
                 {
-                    bool loops = true;
-                    while (loops == true)
+                    Console.WriteLine("Please select from the following options.\n1. Read single byte.\n2. Edit single byte.\n3. Add bytes to end of file.\n4. Quit");
+                    input = Console.ReadLine();
+                    if (input == "1")
                     {
-                        /*long length = editFile.Length;
+                        Console.WriteLine("Apologies but this hasn't been worked on quite yet. Try again later.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        loop = false;
+                    }
+                    else if (input == "2")
+                    {
+                        long length = editFile.Length;
                         Console.WriteLine("The file is exactly 0x" + Convert.ToString(length, 16) + " bytes long. Specify an address to edit one byte.\nPlease only input the number after the '0x'.");
                         value = Console.ReadLine();
-                        bool canParse = Int32.TryParse(value, out valueInt);
-                        if (!canParse)
+                        value = ByteStuff.StringToInt32Ready(value);
+                        valueByte = ByteStuff.StringToByteArray(value);
+                        Console.WriteLine("Bytes converted. You entered: " + valueByte[0].ToString("X") + "\nIs this correct? Y/N");
+                        input = Console.ReadLine();
+                        editFile.Position = BitConverter.ToInt32(valueByte, 0);
+                        if (input == "Y" || input == "y")
                         {
-                            Console.WriteLine("I'm sorry. I couldn't understand your input... Try again?");
-
+                            Console.WriteLine("What do you want to write to this address? Single bytes only. (0-9, A-F)");
+                            input = Console.ReadLine();
+                            if (input.Length <= 2)
+                            {
+                                inputByte = ByteStuff.StringToByteArray(input);
+                                inputInt = inputByte[0];
+                                editFile.WriteByte(inputInt);
+                                Console.WriteLine("Wrote " + inputByte[0].ToString("X") + " to address 0x" + valueByte[0].ToString("X"));
+                            }
+                            loop = false;
                         }
-                        return;*/
+                        else if (input == "N" || input == "n")
+                        {
+                            loop = true;
+                        }
                     }
-
+                    else if (input == "3")
+                    {
+                        Console.WriteLine("Apologies but this hasn't been worked on quite yet. Try again later.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        loop = false;
+                    }
+                    else if (input == "4")
+                    {
+                        loop = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry. I didn't get that. Try again.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        loop = true;
+                    }
                 }
-                else if (input == "2")
-                {
-
-                }
-                else if (input == "3")
-                {
-
-                }
+                while (loop == true);
+                return;
             }
         }
         static void Main(string[] args)
@@ -146,7 +229,8 @@ namespace MethodTraining
 
                     Program EditFile = new Program();
                     EditFile.MethodEditing(nameOfFile);
-
+                    Console.WriteLine("The Program will now close. Press any key to exit...");
+                    Console.ReadKey();
                     return;
                 }
                 else
